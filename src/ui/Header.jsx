41109cffc4cb7de7c07fe16688e16fc742cfaa21
Abduvokh1D <abduvokhideurope@gmail.com>
2025-08-03 08:22:00 +0500

    Fetching orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import Home from "./Home";
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
       <p>Hello Abduvokhid</p>
     </header>
   );
