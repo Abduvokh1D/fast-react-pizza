0de3dcf3163f6f7749db101c1931f3748e40e4f2
Abduvokh1D <abduvokhideurope@gmail.com>
2025-08-10 12:31:38 +0500

    Installing Tailwind CSS and setting up with Prettier

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 import eslint from "vite-plugin-eslint2";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), tailwindcss(),],
 });
