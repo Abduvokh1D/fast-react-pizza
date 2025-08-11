2737c5db508de19c38ecabb43cfbca9018196981
Abduvokh1D <abduvokhideurope@gmail.com>
2025-08-11 13:31:58 +0500

    Syling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
-import Home from "./Home";
 import SearchOrder from "../features/order/SearchOrder.jsx";
+import Username from "../features/user/Username.jsx";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Hello Abduvokhid</p>
+      <Username />
     </header>
   );
 }
