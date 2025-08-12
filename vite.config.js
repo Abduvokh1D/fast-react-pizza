95cf44a7bacffaceae5b24fc80970750d852c7e9
Abduvokh1D <abduvokhideurope@gmail.com>
2025-08-12 12:17:54 +0500

    Absolute position

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 import eslint from "vite-plugin-eslint2";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), tailwindcss(),],
+  plugins: [
+    react(),
+    eslint({ emitWarning: true, emitError: false }),
+    tailwindcss(),
+  ],
 });
