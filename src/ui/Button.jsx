e4263404de3dd429c1ceaae9fa0d81cc750a373e
Abduvokh1D <abduvokhideurope@gmail.com>
2025-10-21 21:33:08 +0500

    Displaying Price and Cart Quantity

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "focus:ring-bg:yellow-300 inline-block text-sm rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed ";
   const styles = {
@@ -14,6 +14,12 @@ function Button({ children, disabled, to, type }) {
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
   return (
     <button disabled={disabled} className={styles[type]}>
