a6fcc7ef30bcdad9cecb7f894dccc48fbd1e7db3
Abduvokh1D <abduvokhideurope@gmail.com>
2025-11-07 09:05:47 +0500

    Making Priority Button and Finishing the Project

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
+import { action as updateOrderAction } from "./features/order/UpdateOrder.jsx";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
       {
         path: "/order/new",
